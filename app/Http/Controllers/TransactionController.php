e81b35d0699d10b4630b0db80d93aeb33b413baa
CindyW03 <hlawinnwe0352@gmail.com>
2024-12-07 00:10:51 +0630

    hlahla

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,19 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Transaction;
 use Illuminate\Http\Request;
-use App\Http\Requests\StoreTransactionRequest;
-use App\Http\Requests\UpdateTransactionRequest;
 
-<<<<<<< HEAD
 class TransactionController extends Controller {
     public function show() {
         $transactions = Transaction::all();
         return view('admin.transactions', compact('transactions'));
     }
 }
-=======
-class TransactionController extends Controller
-{
-    
-}
->>>>>>> 34fed74c39f409fba637f1caf480e5faa4ec3411
+
+
